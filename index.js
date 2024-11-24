bb8ce83e3dcfde7bf6d48d92d5be77b87449b23d
Nwachukwuchinedu <chinedu.simeon2020@gmail.com>
2024-11-24 17:54:02 +0100

    use voteRoute middleware

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import User from "./models/User.js";
+import Position from "./models/Position.js";
+import Vote from "./models/Vote.js";
 import jwt from "jsonwebtoken";
 import nodemailer from "nodemailer";
+import bodyParser from "body-parser";
 import authRoutes from "./routes/authRoutes.js";
+import voteRoutes from "./routes/voteRoute.js";
+
 dotenv.config();
 
 const app = express();
@@ -13,6 +18,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 const connection = mongoose.connect(process.env.MONGODB_URI);
 
@@ -25,6 +31,8 @@ connection.catch((err) => {
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api", voteRoutes);
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
