2006b3f64049f983dac73035a85a5f9fda225a72
Nwachukwuchinedu <chinedu.simeon2020@gmail.com>
2024-11-27 09:42:35 +0100

    added update candidate route

--- routes/voteRoute.js
+++ routes/voteRoute.js
@@ -1,12 +1,24 @@
 import express from "express";
-import { position, getPostions, vote, result } from "../controllers/voteController.js";
+import {
+  position,
+  getPostions,
+  vote,
+  result,
+  updateCandidate,
+} from "../controllers/voteController.js";
+import upload from "../config/multerConfig.js";
 
 const router = express.Router();
 
 // Routes
-router.post("/position/add", position);
+router.post("/position/add", upload.single("picture"), position);
 router.get("/position/all", getPostions);
 router.post("/vote/cast", vote);
 router.get("/vote/result", result);
+router.put(
+  "/position/:id/candidate/update",
+  upload.single("picture"),
+  updateCandidate
+);
 
 export default router;
