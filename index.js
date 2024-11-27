0ed2fa32715c5aeb35857b531e7f8ff1e20bc49f
Nwachukwuchinedu <chinedu.simeon2020@gmail.com>
2024-11-27 08:43:25 +0100

    updated

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ import dotenv from "dotenv";
 import User from "./models/User.js";
 import Position from "./models/Position.js";
 import Vote from "./models/Vote.js";
-import jwt from "jsonwebtoken";
-import nodemailer from "nodemailer";
 import bodyParser from "body-parser";
 import authRoutes from "./routes/authRoutes.js";
 import voteRoutes from "./routes/voteRoute.js";
 
 dotenv.config();
 
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
+
+// Serve static files from the "uploads" directory
+app.use("/uploads", express.static("uploads"));
+
 
 const connection = mongoose.connect(process.env.MONGODB_URI);
 
