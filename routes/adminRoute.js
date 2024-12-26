136b3fbd2f6671027149fc730ce8d6e35ab43588
Nwachukwuchinedu <chinedu.simeon2020@gmail.com>
2024-12-26 11:12:24 +0100

    updated

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,12 +1,31 @@
 import express from "express";
-
+import jwt from "jsonwebtoken";
 import { getCurrentAdmin, logAdminAction, login, signup } from "../controllers/adminController.js";
 
 const router = express.Router();
 
+// Middleware to verify token
+const verifyToken = (req, res, next) => {
+  const token = req.headers["authorization"];
+  if (!token || !token.startsWith("Bearer ")) {
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
+  }
+
+  const actualToken = token.split(" ")[1];
+  jwt.verify(actualToken, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Invalid token." });
+    }
+    req.admin = decoded; // Add user data to request
+    next();
+  });
+};
+
 router.post("/log-admin-action", logAdminAction);
 router.post("/signup", signup);
 router.post("/login", login);
-router.get("/me", getCurrentAdmin);
+router.get("/me",verifyToken, getCurrentAdmin);
 
 export default router;
