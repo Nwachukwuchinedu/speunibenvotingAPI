8e118934873c422ebf7587248c29255cc4bed2a3
Nwachukwuchinedu <chinedu.simeon2020@gmail.com>
2025-01-01 18:33:16 +0100

    added isElectionActive

--- models/Admin.js
+++ models/Admin.js
@@ -3,10 +3,13 @@ import mongoose from "mongoose";
 const adminSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
+    fullname: { type: String, required: true },
+    nickname: { type: String, required: true },
     matno: { type: String, required: true, unique: true },
     level: { type: Number, required: true },
     password: { type: String, required: true },
     verified: { type: Boolean, default: false },
+    electionIsActive: { type: Boolean, default: false },
   },
   {
     timestamps: true,
